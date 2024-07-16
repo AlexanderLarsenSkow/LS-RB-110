4800fbd26481854c2c104e41fc3aa187f8450af7
Alex Skow <askow2015@gmail.com>
2024-07-16 23:58:33 +0000

    Added sort method practice

---
+++ lesson_2/sorting/sort_method.rb
@@ -0,0 +1,14 @@
+# What does this return?
+
+p ['arc', 'bat', 'cape', 'ants', 'cap'].sort
+
+# => ['ants', 'arc', bat', 'cap', 'cape']
+
+p 'arc'.ord # => 97
+p 'bat'.ord # => 98
+p 'cape'.ord # => 99
+p 'ants'.ord # => 97
+p 'cap'.ord # => 99
+
+p 'ants' <=> 'arc'
+p 'cape' <=> 'cap'
