9b4cf7d9af5dd905cd23d04bbdbbc204be453205
Alex Skow <askow2015@gmail.com>
2024-07-28 21:31:29 +0000

    Finished board display with hash values

--- lesson_3/tic_tac_toe/tic_tac_toe.rb
+++ lesson_3/tic_tac_toe/tic_tac_toe.rb
@@ -1,21 +1,21 @@
 
 def display_board(brd)
 	puts "      |           |       "
-	puts " #{brd[1]}     |  #{brd[2]}         | #{brd[3]}      "
+	puts "  #{brd[1]}   |     #{brd[2]}     |   #{brd[3]}      "
 	puts "      |           |       "
 	puts "------+-----------+------ "
 	puts "      |           |       "
-	puts " #{brd[4]}     |  #{brd[5]}         | #{brd[6]}      "
+	puts "  #{brd[4]}   |     #{brd[5]}     |   #{brd[6]}      "
 	puts "      |           |       "
 	puts "------+-----------+------ "
 	puts "      |           |       "
-	puts " #{brd[7]}     | #{brd[8]}          | #{brd[9]}      "
+	puts "  #{brd[7]}   |     #{brd[8]}     |   #{brd[9]}      "
 	puts "      |           |       "
 end 
 
 def initialize_board
 	new_board = {}
-	(1..9).each {|num| new_board[num] = ' '}
+	(1..9).each {|num| new_board[num] = 'X'}
 	new_board
 end 
 
