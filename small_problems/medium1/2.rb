8aa6e7f6fad69dcbcad3e6f5082889d181ffb4db
Alex Skow <askow2015@gmail.com>
2024-08-11 03:32:50 +0000

    Finished Exercise 2

--- small_problems/medium1/2.rb
+++ small_problems/medium1/2.rb
@@ -1,41 +1,66 @@
-# Rotation Part Two
-
 # PEDAC
 
 # Problem:
-	# Expected Input: integer and n digits to be rotated
-	# Expected Output: rotated integer value
+	# Expected Input: Array
+	# Expected Output: Array with rotated elements
 	
 	# Rules:
 		# Explicit:
-			# Write a method that rotates n digits from the end of the the array
-			# The digits rotate to the right
-			# if n equals 1, it returns the original integer
-			# if n equals 2, it returns the 2 last digits swapped.
+			# Write a method that rotates the last n digits of an array
+			# If it rotates 1 digit, it returns the same array
+			# If it rotates 2 digits, it rotates the last 2 elements in the array
+			# If it rotates 3 digits, it rotates the last 3 elements in the array.
+			# The rotation always happens 1 to the left. 
 		
 		# Implicit:
+			# Can take a slice from the array and then rotate that.
 	
 # Examples:
-
-# Data Structure: Array
+	# rotate([735291], 5) => [752913]
+	# rotate([735291], 3) => [735912]
+	
+# Data Structure:
+	# Arrays
 
 # Algorithm:
-	# Take the -number of the input position and take the slice from that spot and the positive number of elements.
-	 
+	# Convert the number to an array of digits reversed
+	# Take a slice from the array based on the n digits variable
+	# It should be index position array size - digits with the number of digits as n
+	# Plug this into the rotate array method from last challenge
+	# Take the slice of the first array before the index of the first slice
+	# Add the rotated_slice into this slice. 
+	# Return the array.
 
 # Code:
 
-
-def rotate_rightmost_digits(digits, number)
-	array = digits.digits.reverse
-	slice = array.size - number
-	array_slice = array[0, slice]
-	array_slice + array[-2..]
-end 
+def rotate_array(array)
+	result = []
+	index = 0
 	
-p rotate_rightmost_digits(735291, 1) #== 735291
-p rotate_rightmost_digits(735291, 2) #= 735219
+	array.each do |el|
+		if index > 0 
+			result << el
+		end 
+		index += 1	
+	end 
+	result << array[0]
+end 
+
+
+def rotate_rightmost_digits(number, digits)
+	array = number.digits.reverse
+	index = array.size - digits
+	original_digits = index
+	
+	slice = array[index, digits]
+	rotated_arr = rotate_array(slice)
+	
+	(array[0, original_digits] += rotated_arr).join.to_i
+end 
+
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
 p rotate_rightmost_digits(735291, 3) == 735912
-p rotate_rightmost_digits(735291, 4) #== 732915
-p rotate_rightmost_digits(735291, 5) #== 752913
-p rotate_rightmost_digits(735291, 6) #== 352917
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
