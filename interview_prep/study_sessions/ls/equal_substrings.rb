7a629abf06fb75049998d20ac68ed7ac01b1371f
Alex Skow <askow2015@gmail.com>
2024-09-05 18:11:05 +0000

    Solved equal character substrings

--- interview_prep/study_sessions/ls/equal_substrings.rb
+++ interview_prep/study_sessions/ls/equal_substrings.rb
@@ -1,76 +1,73 @@
+# Second Attempt on this Problem. Definitely a much stronger showing than before!
+
 # Given a string s, return the length of the longest substring between two equal characters,
 # excluding the two characters. If there is no such substring return -1.
 
 # A substring is a contiguous sequence of characters within a string.
 
-# PEDAC
+# Input: string
+# Output: innteger: the longest length of a substring between 2 equal substrings
 
-# Understanding the Problem:
-  # Input:
-    # string argument
+# Rules:
+# return the length between two equal characters in a substring
+# the substring should not include the characters
+# If there are no substrings like this, return - 1.
 
-  # Output:
-    # integer representing the length of the longest substring between two equal characters
+# Examples:
+  # p max_length_between_equal_characters("acbsewb") == 3 # 3 between b and b
+  #  p max_length_between_equal_characters("acbsewba") == 6 # 6 between a and a
+  # p max_length_between_equal_characters("aa") == 0 # no characters between a and a
+  # p max_length_between_equal_characters("cbzxy") == -1 # return -1 if there are no duplicates
 
-  # Explicit Requirements:
-    # Return the length of the longest substring between two characters
-    # Two equal characters might be "a" and "a"
-    # Exclude those characters in the final value
-    # return -1 if there are no duplicates.
 
-  # Examples:
-    # p max_length_between_equal_characters("acbsewb") == 3 # 3 between b and b
-    #  p max_length_between_equal_characters("acbsewba") == 6 # 6 between a and a
-    # p max_length_between_equal_characters("aa") == 0 # no characters between a and a
-    # p max_length_between_equal_characters("cbzxy") == -1 # return -1 if there are no duplicates
+# Data Structures:
+  # Arrays
+  # We want to iterate over each character, and for each character, iterate again over the collection and compare against one another
+  # My first idea is to iterate over number values for the index. So, for the first index, iterate from 0
+    # to the array size - 1.
+  # for the second index, we could iterate from index + 1 to array size - 1, so that we never go backwards.
+  # if the first character is equal to the second character, we take (index 1 + 1,) (to remove that character)
+  # and - index1 from index2, so we get the length of the substring.
 
-  # Data Structure:
-    # Arrays
-    # Can use this to find all the substrings
+    # Note: If we wanted to include the character, we would make the lenght index2 - index1 + 1. however, we do
+      # not want to include it.
 
+    # take the substring and push it into the array
+
+  # At the end, take max by with length length. maybe push - 1 into the array as well.
+
+  # might be a problem with nil on index + 1, but we'll see.
 
   # Algorithm:
-    # Build an array of substrings from the first duplicate to the second duplicate
-      # create an empty substrings array
-      # create a variable index at 0
-      # create a variable slice at 1
-      # take the slice from index to slice
-      #
+    # create an empty substrings array
+
+    # iterate from 0 to the size of the string - 1, thsi is index1
+    # iterate again from index1 + 1 to the size of the string - 1
+    # the index is index + 1
+    # the length is index2 - index1
+    # take the substring at these variables and add it to the substrings array
+    # after iterating, if the array is empty, return - 1
+    # if not empty, take max by string length and return the length
 
 
-    # Find the longest substring
+def max_length_between_equal_characters(string)
+  substrings = []
 
-
-
-  # Code
-
-  def build_substrings(string)
-    substrings = []
-    index = 0
-    slice = 1
-
-    loop do
-      break if index == string.size
-      substrings << string[index, slice]
-
-      if slice == string.size
-        slice = string.size - index
-        index += 1
+  (0...string.size).each do |index1|
+    ((index1 + 1)...string.size).each do |index2|
+      if string[index1] == string[index2]
+        index1 = index1 + 1
+        length = index2 - index1
+        substrings << string[index1, length]
       end
-      slice += 1
-
     end
-    substrings
   end
-
-  p build_substrings('abc') == ['a', 'ab']
-
-  def max_length_between_equal_characters(string)
-
-  end
+  return - 1 if substrings.empty?
+  substrings.max_by { |str| str.length }.length
+end
 
 
   p max_length_between_equal_characters("acbsewb") == 3 # 3 between b and b
-  p max_length_between_equal_characters("acbsewba") == 6 # 6 between a and a
+    p max_length_between_equal_characters("acbsewba") == 6 # 6 between a and a
   p max_length_between_equal_characters("aa") == 0 # no characters between a and a
   p max_length_between_equal_characters("cbzxy") == -1 # return -1 if there are no duplicates
