3bbbb734447d858b939237cf91ef38ce238c95ec
Alex Skow <askow2015@gmail.com>
2024-09-17 02:18:42 +0000

    Add Solution to Problem 12

--- interview_prep/practice_problems/medium_practice/12.rb
+++ interview_prep/practice_problems/medium_practice/12.rb
@@ -48,3 +48,38 @@ p is_pangram?("A wizard’s job is to vex chumps quickly in golf.") == true
 
 my_str = 'Sixty zippers were quickly picked from the woven jute bag.'
 p is_pangram?(my_str) == true
+
+# Approach 2: Using match
+  # it's pretty simple with an alphabet constant.
+  # Let's try with another approach.
+
+  # Have to check to see if each letter occurs one time.
+  # Could do a count for each character
+  # if count of any characters is less than 1, return false.
+  # that uses any with another alphabet constant.
+  # I think that is going to be the most efficient way to solve this. Otherwise, you're getting into trouble iterating over the sentence with no clear way to check if you have all the letters or not.
+  # Iterating over the alphabet itself is just the right way to go here.
+  # Okay so :
+    # you could take a variable at 0.
+    # increment it by 1 every time you see a unique letter.
+    # take the sentence downcased, uniq it, select only characters that match a-z. If the size is 26, it's a pangram. Else, it's not. Excellent.
+
+  # Algorithm:
+    # MAIN METHOD:
+      # see if the sentence downcased, uniqued, with only alphabetic characters selected has 26 characters in it.
+
+  def is_pangram2(sentence)
+    characters = sentence.downcase.chars.uniq.select do |char|
+      char.match?(/[a-z]/)
+    end
+    characters.size == 26
+  end
+
+    p is_pangram2('The quick, brown fox jumps over the lazy dog!') == true
+    p is_pangram2('The slow, brown fox jumps over the lazy dog!') == false
+    p is_pangram2("A wizard’s job is to vex chumps quickly in fog.") == true
+    p is_pangram2("A wizard’s task is to vex chumps quickly in fog.") == false
+    p is_pangram2("A wizard’s job is to vex chumps quickly in golf.") == true
+
+    my_str = 'Sixty zippers were quickly picked from the woven jute bag.'
+    p is_pangram2(my_str) == true
