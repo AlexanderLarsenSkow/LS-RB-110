b006246a108d9615b800bb55156b3fa110f46274
Alex Skow <askow2015@gmail.com>
2024-09-22 23:05:04 +0000

    Solved Cute Anlges within Decent Precision

--- small_problems/easy6/1.rb
+++ small_problems/easy6/1.rb
@@ -5,18 +5,18 @@
 	# Use a degree symbol to rep degrees, a single quote to rep minutes, and a double quote to rep seconds.
 	# A degree has 60 min
 	# A minute has 60 sec
-	
+
 # Understanding the Problem:
 	# Expected Input: float number
-	# Expected Output: string of numbers with symbols 
-	
+	# Expected Output: string of numbers with symbols
+
 	# Rules:
 		# Degrees have degree symbol
 		# Minutes have single quote
 		# Seconds have double quotes
 		# There are 60 minutes in one degree
 		# There are 60 seconds in one minute
-		
+
 # Examples:
 	# dms(30) == %(30°00'00")
 	# dms(76.73) == %(76°43'48")
@@ -24,11 +24,11 @@
 	# dms(93.034773) == %(93°02'05")
 	# dms(0) == %(0°00'00")
 	# dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
-	
+
 # Data Structure: Not sure yet
 
 # Algorithm:
-	# 1. Create variable minutes that is degrees * 60 
+	# 1. Create variable minutes that is degrees * 60
 	# 2. Create variable seconds that is minutes * 60
 	# 3. Minus the integer value from the float to get the decimal
 	# 4. Multiply this value by 60 to get the minutes
@@ -42,19 +42,18 @@ DEGREE_SYM = "\u00B0"
 
 def dms(float)
 	degree = float.to_i
-	
+	degree = 0 if degree == 360
+
 	minutes_decimal = ((float - degree) * 60) # 36
+	minutes_decimal = 0 if float == 360
 	minutes_integer = minutes_decimal.to_i
 	minutes_display = minutes_decimal.round
-	
-	
+
 	seconds = (minutes_decimal - minutes_integer) * 60
 	seconds_integer = seconds.floor
-	
-	
-	
+
 	"#{degree}#{DEGREE_SYM}#{minutes_display}'#{seconds_integer}\""
-end 
+end
 
 puts dms(30) #== %(30°00'00")
 puts dms(76.73) #== %(76°43'48")
